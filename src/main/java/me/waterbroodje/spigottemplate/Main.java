55b9ac44b67afd330fb5816c6d522e55ee1bf289
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 00:53:50 +0100

    Added getListenerManager method

--- src/main/java/me/waterbroodje/spigottemplate/Main.java
+++ src/main/java/me/waterbroodje/spigottemplate/Main.java
@@ -1,14 +1,19 @@
 package me.waterbroodje.spigottemplate;
 
+import me.waterbroodje.spigottemplate.listeners.ListenerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     private static Main instance;
+    private static ListenerManager listenerManager;
 
     @Override
     public void onEnable() {
         instance = this;
+        listenerManager = new ListenerManager();
+
+        listenerManager.register();
     }
 
     @Override
@@ -18,5 +23,9 @@ public final class Main extends JavaPlugin {
 
     public static Main getInstance() {
         return instance;
+    }
+
+    public static ListenerManager getListenerManager() {
+        return listenerManager;
     }
 }
