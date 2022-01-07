a3fc7e458687fe2d401e04919011772077586e0c
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 00:51:43 +0100

    Added a getInstance method

--- src/main/java/me/waterbroodje/spigottemplate/Main.java
+++ src/main/java/me/waterbroodje/spigottemplate/Main.java
@@ -4,13 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main instance;
+
     @Override
     public void onEnable() {
-
+        instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 }
