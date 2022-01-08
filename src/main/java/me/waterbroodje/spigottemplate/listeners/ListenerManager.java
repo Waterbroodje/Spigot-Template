fbf266225802cf6a05b6e44c9c20565ba164f6a2
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 01:05:25 +0100

    Completed the ListenerManager

--- src/main/java/me/waterbroodje/spigottemplate/listeners/ListenerManager.java
+++ src/main/java/me/waterbroodje/spigottemplate/listeners/ListenerManager.java
@@ -1,10 +1,26 @@
 package me.waterbroodje.spigottemplate.listeners;
 
+import me.waterbroodje.spigottemplate.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class ListenerManager {
 
     public void register() {
         String packageName = getClass().getPackage().getName();
 
-
+        for (Class<?> classes : new Reflections(packageName).getSubTypesOf(Listener.class)) {
+            try {
+                Listener listener = (Listener) classes
+                        .getDeclaredConstructor()
+                        .newInstance();
+                Bukkit.getPluginManager().registerEvents(listener, Main.getInstance());
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
