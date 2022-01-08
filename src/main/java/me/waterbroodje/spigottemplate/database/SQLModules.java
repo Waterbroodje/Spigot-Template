bcd0cf9ecdad0209bed67a4fa6f55ea517259ec5
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 01:39:58 +0100

    Added some basic stuff

--- src/main/java/me/waterbroodje/spigottemplate/database/SQLModules.java
+++ src/main/java/me/waterbroodje/spigottemplate/database/SQLModules.java
@@ -1,4 +1,11 @@
 package me.waterbroodje.spigottemplate.database;
 
+import me.waterbroodje.spigottemplate.Main;
+
 public class SQLModules {
+
+    private Main plugin;
+    public SQLModules(Main plugin) {
+        this.plugin = plugin;
+    }
 }
