88766109c10a4057b82bec08fc777ecb339d1f3c
Waterbroodje <81316147+Waterbroodje@users.noreply.github.com>
2022-06-28 15:43:10 +0200

    Removed the old database variables

--- src/main/java/me/waterbroodje/spigottemplate/Main.java
+++ src/main/java/me/waterbroodje/spigottemplate/Main.java
@@ -1,8 +1,5 @@
 package me.waterbroodje.spigottemplate;
 
-import me.waterbroodje.spigottemplate.commands.CommandManager;
-import me.waterbroodje.spigottemplate.database.MySQL;
-import me.waterbroodje.spigottemplate.database.SQLModules;
 import me.waterbroodje.spigottemplate.listeners.ListenerManager;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,23 +7,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
     private static Main instance;
     private static ListenerManager listenerManager;
-    private static CommandManager commandManager;
-    private static MySQL mySQL;
-    private static SQLModules sqlModules;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-
         instance = this;
         listenerManager = new ListenerManager();
-        commandManager = new CommandManager();
-        mySQL = new MySQL();
-        sqlModules = new SQLModules();
 
         listenerManager.register();
-        commandManager.register();
-        mySQL.mysqlSetup();
     }
 
     public static Main getInstance() {
@@ -35,18 +23,6 @@ public final class Main extends JavaPlugin {
 
     public static ListenerManager getListenerManager() {
         return listenerManager;
-    }
-
-    public static MySQL getMySQL() {
-        return mySQL;
-    }
-
-    public static SQLModules getSqlModules() {
-        return sqlModules;
-    }
-
-    public static CommandManager getCommandManager() {
-        return commandManager;
     }
 
     public static String chat(String s) {
